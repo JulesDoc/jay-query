164d7e89bfe0c7aa161add3606a29682124eab28
Calvo <julio.calvo@itu.int>
2022-04-26 12:17:37 +0200

    First commit

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+/* eslint-disable linebreak-style */
 'use strict';
 
 function J$ (selector) {
-
+  return Object.assign(document.querySelectorAll(selector), methods);
 }
+
+J$.ready = function (func) {
+  document.addEventListener('DOMContentLoaded', func);
+};
 
 // Allow tests to run on the server (leave at the bottom)
 if (typeof window === 'undefined') {
